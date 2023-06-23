c3198e31cd58bbf9c9c4d9bed164d2d5236a7c75
Jul1o21 <137314058+CubasJ@users.noreply.github.com>
2023-06-23 02:48:06 -0500

    summary
    Cambios en los datos superiores

--- src/app/principal-component/datos-superior-componente/datos-superior-componente.component.ts
+++ src/app/principal-component/datos-superior-componente/datos-superior-componente.component.ts
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosSuperiorComponenteComponent implements OnInit {
 
+  
+  selectedItemPredios: string = '';
+  selectedItemMeses: string = '';
+
+  searchValuePredios: string = '';
+  searchValueMeses: string = '';
+
+
+  filteredItemsPredios: string[] = [];
+  filteredItemsMeses: string[] = [];
+
+  isActivePredios: boolean = false;
+  isActiveMeses: boolean = false;
+
+  predios = ["Afghanistan", "Algeria", "Argentina", "Australia", "Bangladesh", "Belgium", "Bhutan",
+    "Brazil", "Canada", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
+    "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
+    "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
+    "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
+    "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+
+  meses = ["Enero-23", "Febrero-23", "Marzo-23", "Abril-23"];
+
   constructor() { }
 
   ngOnInit() {
+    this.filteredItemsPredios = this.predios;
+    this.selectedItemPredios = this.predios[0];
+
+    this.filteredItemsMeses = this.meses;
+    this.selectedItemMeses = this.meses[0];
+  }
+
+  toggleActivePredios(): void {
+    this.isActivePredios = !this.isActivePredios;
+  }
+
+  toggleActiveMeses(): void {
+    console.log("se detecta el click");
+    this.isActiveMeses = !this.isActiveMeses;
+  }
+
+  selectItemPredios(item: string): void {
+    this.selectedItemPredios = item;
+    this.isActivePredios = false;
+  }
+
+  selectItemMeses(item: string): void {
+    this.selectedItemMeses = item;
+    this.isActiveMeses = false;
+  }
+
+  filterItemsPredios(): void {
+    this.filteredItemsPredios = this.predios.filter(item => item.toLowerCase().startsWith(this.searchValuePredios.toLowerCase()));
+  }
+
+  filterItemsMeses(): void {
+    this.filteredItemsMeses = this.meses.filter(item => item.toLowerCase().startsWith(this.searchValueMeses.toLowerCase()));
   }
 
 }
--- src/app/principal-component/principal-component.component.ts
+++ src/app/principal-component/principal-component.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponentComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
